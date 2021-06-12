e3e2f4f1af577d45eefc87fb4daa61f14088d61b
vikashkrs <39235627+vikashkrs@users.noreply.github.com>
2021-06-12 15:28:20 -0500

    changed the datatype of songplay_id column

--- sql_queries.py
+++ sql_queries.py
@@ -8,7 +8,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays(songplay_id varchar PRIMARY KEY,start_time timestamp,user_id varchar,level varchar, song_id varchar, artist_id varchar, session_id varchar, location varchar, user_agent varchar)""")
+songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays(songplay_id SERIAL PRIMARY KEY,start_time timestamp,user_id varchar,level varchar, song_id varchar, artist_id varchar, session_id varchar, location varchar, user_agent varchar)""")
 
 user_table_create = (""" CREATE TABLE IF NOT EXISTS users(user_id varchar PRIMARY KEY, first_name varchar, last_name varchar, gender text,level varchar)""")
 
@@ -16,13 +16,19 @@ song_table_create = (""" CREATE TABLE IF NOT EXISTS songs(song_id varchar PRIMAR
 
 artist_table_create = (""" CREATE TABLE IF NOT EXISTS artists(artist_id varchar PRIMARY KEY, name varchar, location varchar, latitude varchar, longitude varchar)""")
 
-time_table_create = (""" CREATE TABLE IF NOT EXISTS time(start_time timestamp PRIMARY KEY, hour int, day int, week int, month int, year int, weekday varchar) 
-""")
+time_table_create = (""" CREATE TABLE IF NOT EXISTS time(
+start_time timestamp PRIMARY KEY, 
+hour int, 
+day int, 
+week int, 
+month int, 
+year int, 
+weekday varchar)""")
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""INSERT INTO songplays (songplay_id,start_time,user_id,level,song_id,artist_id,session_id,location,
-user_agent) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s) ON CONFLICT (songplay_id) DO NOTHING""")
+songplay_table_insert = ("""INSERT INTO songplays (start_time,user_id,level,song_id,artist_id,session_id,location,
+user_agent) VALUES (%s,%s,%s,%s,%s,%s,%s,%s) ON CONFLICT (songplay_id) DO NOTHING""")
 
 user_table_insert = ("""INSERT INTO users (user_id,first_name,last_name,gender,level) VALUES (%s,%s,%s,%s,%s) ON CONFLICT (user_id) DO NOTHING
 """)
@@ -37,7 +43,7 @@ time_table_insert = ("""INSERT INTO time(start_time,hour,day,week,month,year,wee
 
 # FIND SONGS
 
-song_select = ("""SELECT * from songs LIMIT 5""")
+song_select = ("""SELECT song_id,artist_id from songs LIMIT 5""")
 
 # QUERY LISTS
 
